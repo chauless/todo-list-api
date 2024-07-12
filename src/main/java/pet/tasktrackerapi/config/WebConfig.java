b4d5eeeb2f7ee37a0e7846df33e9083b92a63f2c
chopchaper42 <vorongri@fel.cvut.cz>
2024-07-12 14:19:18 +0200

    interceptor functionality added

---
+++ src/main/java/pet/tasktrackerapi/config/WebConfig.java
@@ -0,0 +1,19 @@
+package pet.tasktrackerapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import pet.tasktrackerapi.interceptors.CustomInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private CustomInterceptor customInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(customInterceptor);
+    }
+}
